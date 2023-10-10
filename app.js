c333d67a2acc1576255e132725cc56d4aa7017db
talalsidd94 <144280751+talalsidd94@users.noreply.github.com>
2023-10-10 13:15:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port : 3000");
+app.listen(80, () => {
+  console.log("Listening on port : 80");
 });
